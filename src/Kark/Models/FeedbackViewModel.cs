9211febf8354781444e7d2cc0c6a3911c171b7c8
mtkachenko <oblomov86@gmail.com>
2018-06-07 20:44:15 +0300

    added initial aspnetcore project for new kark

---
+++ src/Kark/Models/FeedbackViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Kark.Models
+{
+    //todo need validation
+    public class FeedbackViewModel
+    {
+        public string SubmitterEmail { get; set; }
+
+        public string Text { get; set; }
+    }
+}
