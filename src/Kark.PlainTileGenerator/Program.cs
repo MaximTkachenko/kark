419f43c456566290148b60abd91c3603609e9169
mtkachenko <oblomov86@gmail.com>
2019-05-19 16:57:23 +0200

    removed useless files and projects + added tool to generate plain tiles

---
+++ src/Kark.PlainTileGenerator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Kark.PlainTileGenerator
+{
+    class Program
+    {
+        private const int TileSize = 91;
+
+        static void Main(string[] args)
+        {
+            var tiles = JsonConvert.DeserializeObject<Dictionary<string, Tile>>(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tiles.json")));
+            var folderToSave = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tiles");
+            if (Directory.Exists(folderToSave))
+            {
+                Directory.Delete(folderToSave);
+            }
+
+            foreach (var tile in tiles)
+            {
+                using (Bitmap bmp = new Bitmap(TileSize, TileSize))
+                {
+
+                    bmp.Save(Path.Combine(folderToSave, $"{tile.Key}.png"), ImageFormat.Png);
+                }
+            }
+
+            /*int width = 512;
+            int height = 512;
+
+            int x, y, w, h;
+            x = y = 10;
+            w = h = 100;
+
+            using (Bitmap bmp = new Bitmap(width, height))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+
+                    g.FillRectangle(
+                        brush: new SolidBrush(
+                            color: Color.Blue
+                        ),
+                        rect: new Rectangle(x, y, w, h)
+                    );
+
+                    g.DrawRectangle(
+                        pen: new Pen(
+                            color: Color.Black,
+                            width: 3
+                        ),
+                        rect: new Rectangle(x, y, w, h)
+                    );
+
+                    bmp.Save(@"C:\temp\result.png", ImageFormat.Png);
+
+                }
+            }*/
+
+        }
+    }
+}
---
+++ src/Kark.PlainTileGenerator/Tile.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Kark.PlainTileGenerator
+{
+    class Tile
+    {
+        [JsonProperty("content")]
+        public string[][] Content { get; set; }
+    }
+}
