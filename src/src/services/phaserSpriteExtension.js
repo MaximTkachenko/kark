65ff0df2ccf98c19e8504f7646be716b18d995c0
mtkachenko <oblomov86@gmail.com>
2020-06-14 22:20:39 +0200

    moved old source into another folder

--- src/src/services/phaserSpriteExtension.js
+++ src/src/services/phaserSpriteExtension.js
@@ -1,5 +1,4 @@
-﻿import Phaser from "phaser";
-import Constants from "./globalConstants";
+﻿import Constants from "./globalConstants";
 import MetadataCalculator from "./metadataCalculator";
 
 (function (tileProto) {
